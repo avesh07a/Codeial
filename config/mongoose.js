918ede7b3a1a01f1dedd9adb5672ffdbd97ffd1e
Mohd Avesh Farooqui <Aveshfarooqui07@gmail.com>
2021-04-23 11:47:35 +0530

    Successfully connected to database

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+const { connect } = require('../routes');
+mongoose.connect('mongodb://localhost/test');
+
+const db=mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('successfully conne cted to database');
+
+});
+
+
